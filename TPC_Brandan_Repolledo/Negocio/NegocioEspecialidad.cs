564ece97e2a58deac55f778c6ca4c0ddd7236022
MaiteRodas <wendyrr28@gmail.com>
2020-11-23 00:03:02 -0300

    Se intenta agregar funcionalidad de alta de Especialidades, modificar

--- TPC_Brandan_Repolledo/Negocio/NegocioEspecialidad.cs
+++ TPC_Brandan_Repolledo/Negocio/NegocioEspecialidad.cs
@@ -9,31 +9,6 @@ namespace Negocio
 {
     public class NegocioEspecialidad
     {
-        /* public List<Especialidad> ListaEspecialidad()
-         {
-             List<Especialidad> ListaEspecialidad = new List<Especialidad>();
-             AccesoDatos datos = new AccesoDatos();
-             try
-             {
-                 datos.SetearQuery("select LegajoMedico, DNI, FechaIngreso, Especialidad from Medico as M inner join Usuario as u on u.DNI=M.DNI");
-                 datos.EjecutarConsulta();
-                 while (datos.Lector.Read())
-                 {
-                     Especialidad aux = new Especialidad();
-
-                     aux.LegajoMedico = datos.Lector.GetString(0);
-                     aux.FechaIngreso = datos.Lector.GetDateTime(1);
-                     // aux.Especialidad = datos.Lector.GetString(3);
-                     // me parece que me falta un dato.
-                     ListaEspecialidad.Add(aux);
-                 }
-             }
-             catch (Exception ex)
-             {
-                 throw ex;
-             }
-             return ListaEspecialidad;
-         }
 
          public List<Especialidad> ListaEspecialidades()
          {
@@ -59,40 +34,28 @@ namespace Negocio
              return ListaEspecialidades;
          }
 
-         public void AgregarPersona(Persona nuevo)
+         public void AgregarEspecialidad(Especialidad nuevo)
          {
              AccesoDatos datos = new AccesoDatos();
-             datos.SetearQuery("insert into Persona (DNI,Nombre,Apellido,Domicilio,FechaNacimiento,Genero,Estado) values (@DNI,@Nombre,@Apellido,@Domicilio,@FechaNacimiento,@Genero,@Estado);");
-             datos.AgregarParametro("@DNI", nuevo.DNI);
+             datos.SetearQuery("insert into Especialidad (IdEspecialidad, Nombre, Descripcion) values (@IdEspecialidad, @Nombre, @Descripcion);");
+             datos.AgregarParametro("@IdEspecialidad", nuevo.IdEspecialidad);
              datos.AgregarParametro("@Nombre", nuevo.Nombre);
-             datos.AgregarParametro("@Apellido", nuevo.Apellido);
-             datos.AgregarParametro("@Domicilio", nuevo.Domicilio);
-             datos.AgregarParametro("@FechaNacimiento", nuevo.FechaNacimiento);
-             datos.AgregarParametro("@Genero", nuevo.Genero);
-             datos.AgregarParametro("@Estado", nuevo.Estado);
+             datos.AgregarParametro("@Descripcion", nuevo.Descripcion);
              datos.EjecutarConsulta();
 
          }
 
-         public void AgregarMedico(Medico nuevo, Persona Persona, Especialidad idEsp)
+
+         public void ModificarEspecialidad(Especialidad nuevo)
          {
              AccesoDatos datos = new AccesoDatos();
-             datos.SetearQuery("insert into Medico (LegajoMedico,DNI,FechaIngreso,Especialidad) values (@LegajoMedico, @DNI, @FechaIngreso, @Especialidad);");
-             datos.AgregarParametro("@LegajoMedico", nuevo.LegajoMedico);
-             datos.AgregarParametro("@DNI", Persona.DNI);
-             datos.AgregarParametro("@FechaIngreso", nuevo.FechaIngreso);
-             datos.AgregarParametro("@Especialidad", idEsp.IdEspecialidad);
-             datos.EjecutarConsulta();
+             datos.SetearQuery("update Especialidad set DNI=@DNI, Especialidad=@Especialidad where DNI = @DNI");
+            datos.AgregarParametro("@IdEspecialidad", nuevo.IdEspecialidad);
+            datos.AgregarParametro("@Nombre", nuevo.Nombre);
+            datos.AgregarParametro("@Descripcion", nuevo.Descripcion);
+            datos.EjecutarConsulta();
          }
 
-         public void ModificarMedico(Medico nuevo)
-         {
-             AccesoDatos datos = new AccesoDatos();
-             datos.SetearQuery("update Medico set DNI=@DNI, Especialidad=@Especialidad where DNI = @DNI");
-             datos.AgregarParametro("@Especialidad", nuevo.Especialidad);
-             datos.EjecutarConsulta();
-         }
-
-     }*/
+     }
     }
 }
